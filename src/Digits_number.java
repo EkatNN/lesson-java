7db6f2c11c0cc09f02245de22ce482464a6c381f
EkatNN <nikolaevakate17@gmail.com>
2022-02-28 16:42:15 +0700

    Update file Digits_number

--- src/Digits_number.java
+++ src/Digits_number.java
@@ -3,31 +3,44 @@ import java.util.Scanner;
 public class Digits_number {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите ваше число: ");
+        System.out.print("Введите ваше число: ");
         int number = scanner.nextInt();
         System.out.println(getAllSum(number));
         System.out.println(getOddSum(number));
+        System.out.println(maxDigit(number));
     }
     public static int getAllSum(int number){
-        int sum =0;
-        while(number!= 0){
-            sum+=(number%10);
-            number/=10;
+        int sum = 0;
+        while(number != 0){
+            sum += (number%10);
+            number /= 10;
         }
         return sum;
     }
 
     public static int getOddSum (int number){
-        int oddSum =0;
+        int oddSum = 0;
         int count = 0;
         while(number!=0){
             oddSum = number%10;
-            number/=10;
-            if (oddSum%2!=0){
-                count +=oddSum;
+            number /= 10;
+            if (oddSum%2 != 0){
+                count += oddSum;
             }
         }
         return count;
+    }
+    public static int maxDigit (int number){
+        int biggest = number%10;
+        number /= 10;
+        while (number > 0)  { 
+        int lastDigit = number % 10;
+            if (lastDigit > biggest)  { 
+                biggest = lastDigit;
+            } 
+        number /= 10;
+        }
+        return biggest; 
     }
 }
 
