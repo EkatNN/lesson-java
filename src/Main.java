900737d5eb4ac705d52841b859d205f936dbb5c1
EkatNN <nikolaevakate17@gmail.com>
2022-02-28 16:41:36 +0700

    Add 2files: Person and Mailn. Update Digits_number

---
+++ src/Main.java
@@ -0,0 +1,4 @@
+public class Main{
+    Person person1 = new Person("Ivanov", "Ivan", "Ivanovich", 20);
+
+}
---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name;
+    private String middleName;
+    private String familyName;
+    private int age;
+
+    public Person (String familyName, String middleName, String name, int age) {
+        this.familyName = familyName;
+        this.middleName = middleName;
+        this.name = name;
+        this.age = age;
+    
+    }
+    public void print(){
+        
+    }
+}
